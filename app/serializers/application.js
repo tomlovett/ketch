a441b49b239c35fd90d82c672c3f412c41fe5ab3
Tom Lovett <tomlovett6@gmail.com>
2018-08-21 10:42:32 -0400

    serialize attrs/relationships

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -2,10 +2,34 @@ import { underscore } from '@ember/string';
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
+  serialize(snapshot, options) {
+    let json = this._super(...arguments)
+
+    json.data.attributes = this.underscoreKeys(json.data.attributes)
+    json.data.relationships = this.underscoreKeys(json.data.relationships)
+
+    return json;
+  },
+
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
     console.log('normalizeResponse -> payload: ', payload);
     const normalizedResponse = this._super(...arguments);
     console.log('normalizedResponse: ',normalizedResponse)
     return normalizedResponse
+  },
+
+  underscoreKeys(obj) {
+    let k, underscored;
+
+    for (k in obj) {
+      underscored = k.underscore()
+
+      if (k !== underscored) {
+        obj[underscored] = obj[k]
+        delete obj[k]
+      }
+    }
+
+    return obj
   }
 });
