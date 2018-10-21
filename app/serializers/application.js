9bfadaf1a96f95469718f56c305a53050d322968
Tom Lovett <tomlovett6@gmail.com>
2018-10-21 14:54:15 -0400

    Remove uneccesary param modifications

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,27 +1,4 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
-  serialize(snapshot, options) {
-    let json = this._super(...arguments)
-
-    json.data.attributes = this.underscoreKeys(json.data.attributes) // has to be updated
-    json.data.relationships = this.underscoreKeys(json.data.relationships)
-
-    return json
-  },
-
-  underscoreKeys(obj) {
-    let k, underscored;
-
-    for (k in obj) {
-      underscored = k.underscore()
-
-      if (k !== underscored) {
-        obj[underscored] = obj[k]
-        delete obj[k]
-      }
-    }
-
-    return obj
-  }
 });
