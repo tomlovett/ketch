1d3dfcab641ccc4c1825f1d1ee5c85830dbdea91
Tom Lovett <tomlovett6@gmail.com>
2019-03-17 22:37:34 -0400

    change team.user from belongsTo to hasMany

--- app/models/team.js
+++ app/models/team.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
-const { attr, belongsTo, hasMany } = DS;
+const { attr, hasMany } = DS;
 
 export default DS.Model.extend({
   game: hasMany(),
@@ -8,5 +8,5 @@ export default DS.Model.extend({
   players: hasMany(),
   primary: attr('string'),
   secondary: attr('string'),
-  user: belongsTo(),
+  user: hasMany(),
 });
